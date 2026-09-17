be482d1e13bee27a6ec1a6335535a818ba4128bb
Tami Hartshorn <thartshorn@gavant.com>
2026-09-17 15:05:29 -0400

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using DSharpPlus;
+using DSharpPlus.Entities;
+
+namespace RecurringSignup;
+
+public class Program
+{
+    static async Task Main(string[] args)
+    {
+        var config = new DiscordConfiguration()
+        {
+            Token = "",
+            Intents = DiscordIntents.AllUnprivileged | DiscordIntents.MessageContents
+        };
+
+        var client = new DiscordClient(config);
+        var status = new DiscordActivity("with fire", ActivityType.Playing);
+
+        client.MessageCreated += async (s, e) => 
+        {
+            if (e.Message.Content.ToLower().StartsWith("ping"))
+            {
+                await e.Message.RespondAsync("pong!");
+            }
+        };
+
+        await client.ConnectAsync(status, UserStatus.Online);
+        await Task.Delay(-1);
+    }
+}
---
+++ TOS.md
@@ -0,0 +1,3 @@
+### Terms of Service ###
+By installing this bot on your discord server you agree not to use it to commit crimes or harm others.
+Thanks.
